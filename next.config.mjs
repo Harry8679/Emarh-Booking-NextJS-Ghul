7eb368b3134870b06ddebba63ca13157c7f31b31
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-17 02:42:07 +0200

    Link from mongoDB

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        DB_LOCAL_URI: 'mongodb+srv://harry86:Davidvilla7@ghulam-abbas.taez9.mongodb.net/emarh-booking?retryWrites=true&w=majority&appName=Ghulam-Abbas',
+        DB_URI: 'mongodb+srv://harry86:Davidvilla7@ghulam-abbas.taez9.mongodb.net/emarh-booking?retryWrites=true&w=majority&appName=Ghulam-Abbas',
+    }
+};
 
 export default nextConfig;
