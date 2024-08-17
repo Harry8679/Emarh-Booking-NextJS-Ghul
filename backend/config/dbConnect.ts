236fedddda604c61e19064581fccaf3f23a815c2
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-17 02:49:33 +0200

    Connection to MongoDB

--- backend/config/dbConnect.ts
+++ backend/config/dbConnect.ts
@@ -12,7 +12,7 @@ const dbConnect = async () => {
     if (process.env.NODE_ENV === 'development') DB_URI = process.env.DB_LOCAL_URI!;
     if (process.env.NODE_ENV === 'production') DB_URI = process.env.DB_URI!;
 
-    await mongoose.connect(DB_URI);
+    await mongoose.connect(DB_URI).then((con) => console.log('DB Connected'));
 };
 
 export default dbConnect;
